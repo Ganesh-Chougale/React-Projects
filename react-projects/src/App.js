dc5b9b13bcb185845a3d62087ba6731eb62b6dbf
gchougale <gchougale32@gmail.com>
2024-11-09 13:57:24 +0530

    saved

---
+++ react-projects/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Temp from './Projects/Temp';
+
+function App() {
+  return (
+    <div className="App">
+    <Temp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-projects/src/Projects/Temp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Temp() {
+  return (
+    <div>
+    <h1>jai Shree Ram</h1>      
+    </div>
+  )
+}
+
+export default Temp
